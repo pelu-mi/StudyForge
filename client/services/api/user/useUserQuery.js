74d07e3e8cf867dbe5deb206e998b3782c65d741
James Kanin <kaninnoothep@gmail.com>
2024-11-15 02:15:26 -0600

    add get user

---
+++ client/services/api/user/useUserQuery.js
@@ -0,0 +1,11 @@
+import { GET_USER_API_KEY } from "@/services/constants";
+import { useApiGet } from "@/services/hooks/useApiGet";
+
+export const useUserQuery = (options) => {
+  const { data, ...rest } = useApiGet([GET_USER_API_KEY], options);
+
+  return {
+    ...rest,
+    user: data?.data ?? {},
+  };
+};
--- client/services/constants.js
+++ client/services/constants.js
@@ -1,4 +1,5 @@
 // User Routes
 export const SIGN_UP_API_KEY = "/user/createaccount";
 export const LOGIN_API_KEY = "/user/login";
+export const GET_USER_API_KEY = "/user/getuser";
 export const UPDATE_USER_API_KEY = "/user/updateuser";
