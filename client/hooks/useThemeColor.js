db7cf2cfc6d938d753c516a278c95a0a52eb5a0d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2024-12-13 09:52:35 -0600

    Added comments for client hooks

--- client/hooks/useThemeColor.js
+++ client/hooks/useThemeColor.js
@@ -3,10 +3,20 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
+/**
+ * Import Modules
+ */
 import { useColorScheme } from "react-native";
 
 import { Colors } from "@/constants/Colors";
 
+/**
+ * useThemeColor
+ * 
+ * @param {*} props 
+ * @param {*} colorName 
+ * @returns useThemeColor
+ */
 export function useThemeColor(props, colorName) {
   const theme = useColorScheme() ?? "light";
   const colorFromProps = props[theme];
